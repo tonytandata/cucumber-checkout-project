1f1d08d3b11fcf7d0f0dfaea6140b5dbca176775
Tony Tan <tony.tan@superchoice.com.au>
2023-06-12 22:29:52 +1000

    Creating step definitions and updating script to compile the class first

--- cucumber.sh
+++ cucumber.sh
@@ -1,1 +1,2 @@
-java -cp "jars/*" io.cucumber.core.cli.Main -p pretty .
+javac -cp "jars/*" step_definitions/CheckoutSteps.java
+java -cp "jars/*:." io.cucumber.core.cli.Main -p pretty --snippets camelcase -g step_definitions features
---
+++ step_definitions/CheckoutSteps.java
@@ -0,0 +1,24 @@
+package step_definitions;
+
+import io.cucumber.java.en.*;
+import io.cucumber.java.PendingException;
+
+public class CheckoutSteps {
+    @Given("the price of a {string} is 40c")
+    public void thePriceOfAIs40c(String string) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("I checkout {int} {string}")
+    public void iCheckout(Integer int1, String string) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("the total price should be 40c")
+    public void theTotalPriceShouldBe40c() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
