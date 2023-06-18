0bf69c8b4775e96616d8f38278230df8d7d08b85
Tony Tan <tonyyjt@gmail.com>
2023-06-18 19:24:10 +1000

    Implement Checkout class and transform checkout scenario to scenario outline

--- implementation/Checkout.java
+++ implementation/Checkout.java
@@ -1,10 +1,12 @@
 package implementation;
 
 public class Checkout {
+    int runningTotal = 0;
     public void add(int count, int price){
+        runningTotal += (count * price);
     }
 
     public Integer total(){
-        return 0;
+        return runningTotal;
     }
 }
